0eb6841cd9cfdfa00018468ce7a23cd9ebb3cba7
蛸坊主/たこぼうず <takobouzu@mbf.nifty.com>
2021-02-09 22:14:05 +0900

    Add files via upload

--- 310_tools/110_m_pool_code/m_pool_code_import.sh
+++ 310_tools/110_m_pool_code/m_pool_code_import.sh
@@ -1,3 +1,3 @@
 echo 開始 m_pool_code.csv `date`
-sqlite3 -separator , /home/pi/BOAT_RACE_DB/230_db/boatrace.dbboatrace.db ".import  /home/pi/BOAT_RACE_DB//110_m_pool_code/m_pool_code.csv m_pool_code"
+sqlite3 -separator , /home/pi/BOAT_RACE_DB/230_db/boatrace.db ".import  /home/pi/BOAT_RACE_DB/310_tools/110_m_pool_code/m_pool_code.csv m_pool_code"
 echo 終了 `date`
--- 310_tools/120_m_player/m_player_import.sh
+++ 310_tools/120_m_player/m_player_import.sh
@@ -1,3 +1,3 @@
 echo 開始 m_player.csv `date`
-sqlite3 -separator , /home/pi/BOAT_RACE_DB/230_db/boatrace.dbboatrace.db ".import  /home/pi/BOAT_RACE_DB//110_m_pool_code/m_player_code.csv m_player"
+sqlite3 -separator , /home/pi/BOAT_RACE_DB/230_db/boatrace.db ".import  /home/pi/BOAT_RACE_DB/310_tools/120_m_player/m_player.csv m_player"
 echo 終了 `date`
